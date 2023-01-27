acf257e5a10fa93c87352d24005976a08cffdd73
Chandrika Vennapusa <chandrikavennapusa@gmail.com>
2023-01-27 18:09:06 +0530

    graphs and storage

---
+++ store.js
@@ -0,0 +1,18 @@
+
+function formsubmit(){
+ document.getElementById('name1').innerHTML  =document.getElementById("name").value;
+ document.getElementById('name11').innerHTML = document.getElementById("namee").value;
+ document.getElementById('email1').innerHTML = document.getElementById("email").value;
+ document.getElementById('ph1').innerHTML = document.getElementById("ph").value;
+ document.getElementById('password1').innerHTML =document.getElementById("password").value;
+ document.getElementById('DOB1').innerHTML = document.getElementById("DOB").value;
+ document.getElementById('Gender1').innerHTML = document.getElementById("female").value;
+ document.getElementById('Gender1').innerHTML = document.getElementById("custom").value;
+ document.getElementById('Gender1').innerHTML = document.getElementById("male").value;
+ document.getElementById('file1').innerHTML =document.getElementById("file").value;
+ document.getElementById('Course1').innerHTML = document.getElementById("Course").value;
+ document.getElementById('mode11').innerHTML = document.getElementById("mode").value;
+ document.getElementById('mode11').innerHTML = document.getElementById("mode1").value;
+ document.getElementById('text1').innerHTML = document.getElementById("text").value;      
+}
+
